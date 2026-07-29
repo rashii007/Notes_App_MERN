5f5bd24fffbd75a9249f9bec6b399004530c913c
Muhammad Rashid Khan <pmik754@gmail.com>
2026-07-29 23:15:49 +0100

    Rename themeContext to ThemeContext

--- Frontend/src/context/themeContext.jsx
+++ Frontend/src/context/themeContext.jsx
@@ -15,7 +15,7 @@ export const ThemeProvider = ({ children }) => {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
     }
-  }, [darkMode]);
+  }, [darkMoe]);
 
   const toggleTheme = () => {
     setDarkMode((prev) => !prev);
